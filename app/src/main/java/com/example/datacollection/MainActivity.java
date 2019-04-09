7880b407e2cfa4a131f911196b7e04ee9e592b98
vaibnak <vaibnakgupta100@gmail.com>
2019-04-09 20:18:38 +0530

    added authentication

--- app/src/main/java/com/example/datacollection/MainActivity.java
+++ app/src/main/java/com/example/datacollection/MainActivity.java
@@ -1,24 +1,38 @@
 package com.example.datacollection;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText nameText,emailText,passText;
-    private Button signup,login;
+    private Button regadmin,reguser;
     String name, email, pass;
+    FirebaseAuth mAuth;
+
 
     public void instantiate(){
+
         nameText = findViewById(R.id.name);
         emailText = findViewById(R.id.email);
         passText = findViewById(R.id.pass);
-        signup = findViewById(R.id.sup);
-        login = findViewById(R.id.log);
+        regadmin = findViewById(R.id.sup);
+        reguser = findViewById(R.id.log);
+        mAuth = FirebaseAuth.getInstance();
+
     }
     public void takeValue(){
         name = nameText.getText().toString();
@@ -30,14 +44,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         instantiate();
-        takeValue();
     }
 
     public void login(View view) {
-        Toast.makeText(this, "login", Toast.LENGTH_SHORT).show();
+
+        takeValue();
+        mAuth.signInWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     public void signup(View view) {
-        Toast.makeText(this,"signup",Toast.LENGTH_SHORT).show();
+
+        mAuth = FirebaseAuth.getInstance();
+        takeValue();
+        mAuth.createUserWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
+
+
 }
